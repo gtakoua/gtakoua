ce55fc9224c5cf2b378c71f89d124a4b8bff9295
hanin morgene <morjenehanin1@gmail.com>
2024-02-01 20:55:04 +0100

    portfolio

---
+++ index.js
@@ -0,0 +1,18 @@
+const button = document.getElementById('btn')
+
+
+button.addEventListener('click', (e) => {
+    alert('hanine morgen')
+})
+
+const nameA = document.getElementById('hanin')
+
+nameA.addEventListener('click', (e) => {
+     alert('MY NAME IS HANIN')
+})
+
+
+const button2 = document.getElementById('button2')
+button2.addEventListener('click', (e) =>{
+       alert('Hello Hanin')
+})
